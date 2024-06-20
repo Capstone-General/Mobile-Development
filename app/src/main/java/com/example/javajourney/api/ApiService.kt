e730480f5d70a8c02f40d82769babc51e5bf5d27
septiannaim <naimplung28@gmail.com>
2024-06-20 08:08:02 +0700

    update-detail

---
+++ app/src/main/java/com/example/javajourney/api/ApiService.kt
@@ -0,0 +1,4 @@
+package com.example.javajourney.api
+
+interface ApiService {
+}
---
+++ app/src/main/java/com/example/javajourney/api/Places.kt
@@ -0,0 +1,18 @@
+package com.example.javajourney.api
+
+data class Places(
+    val category: String,
+    val city: String,
+    val description: String,
+    val imageUrl: String,
+    val phone: String,
+    val place: String,
+    val place_id: String,
+    val price: String,
+    val rating: Double,
+    val sites: String,
+    val travel1: String,
+    val travel2: String,
+    val travel3: String,
+    val travel4: String
+)
